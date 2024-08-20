20f54d83a1b83fd87267d6938926cfa68fa807cf
Armolas <armolas06@gmail.com>
2024-08-20 23:39:26 +0100

    created models

---
+++ backend/api/app/models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from .user import User
+from .provider import Provider
+from .service import Service
+from .booking import Booking
+from .review import Review
+from .session import Session
